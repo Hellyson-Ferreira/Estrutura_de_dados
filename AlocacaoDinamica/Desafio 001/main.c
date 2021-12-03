0cd8e6360ea129eb45421f978b8a0180abcbe33c
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2021-12-03 14:30:16 -0300

    added main.c

--- AlocacaoDinamica/Desafio 001/main.c
+++ AlocacaoDinamica/Desafio 001/main.c
@@ -1,72 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
-void showArray(int index, int array[]) {
-    printf("[");
-    for (int i = 0; i < index; ++i) {
-        printf("%d ", array[i]);
-    }
-    printf("]");
+typedef struct {
+    char rua[32];
+    char complemento[64];
+    char bairro[32];
+    char cep[12];
+    char cidade[32];
+    char estado[32];
+    char pais[32];
+    int numero;
+} Endereco;
 
-}
+typedef struct {
+    int ddd;
+    int numero;
+} Telefone;
 
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} Aniversario;
+
+typedef struct Usuario {
+    char nome[32];
+    char email[32];
+    char curso[8];
+    char observacao[128];
+    Endereco endereco;
+    Telefone telefone;
+    Aniversario aniversario;
+};
 
 int main() {
 
-    int *pointerAges, *pointerUnderAge, *pointerOfAge;
-    int indexAges, atualAge, indexUnderAge = 0, indexOfAge = 0;
-
-    printf("Digite a quntidade de idades que serão cadastradas: ");
-    scanf("%d", &indexAges);
-
-    pointerAges = malloc(indexAges * sizeof(int));
-
-
-    for (int i = 0; i < indexAges; i++) {
-        printf("\nDigite a idade %d: ", i);
-        scanf("%d", &atualAge);
-        pointerAges[i] = atualAge;
-
-        if (atualAge > 17) {
-            indexOfAge++;
-        } else {
-            indexUnderAge++;
-        }
-
-    }
-
-    pointerOfAge = malloc(indexOfAge * sizeof(int));
-
-    pointerUnderAge = malloc(indexUnderAge * sizeof(int));
-
-
-    int auxIndexOfAges = 0;
-    int auxIndexUnderAge = 0;
-    for (int i = 0; i < indexAges; i++) {
-
-        if (pointerAges[i] > 17) {
-            pointerOfAge[auxIndexOfAges] = pointerAges[i];
-            auxIndexOfAges++;
-        } else {
-            pointerUnderAge[auxIndexUnderAge] = pointerAges[i];
-            auxIndexUnderAge++;
-        }
-
-    }
-
-    printf("Idades cadastradas: => ");
-    showArray(indexAges, pointerAges);
-
-    printf("\nIdades maiores que 18: => ");
-    showArray(indexOfAge, pointerOfAge);
-
-    printf("\nIdades menores que 18: => ");
-    showArray(indexUnderAge, pointerUnderAge);
-
-    free(pointerAges);
-    free(pointerOfAge);
-    free(pointerUnderAge);
-
-
-    return 0;
+    return  0;
 }
